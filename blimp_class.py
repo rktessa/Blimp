30f48db5640af81006ba67128a3afd765c5903fb
rktessa <rktessa@gmail.com>
2022-12-05 10:50:58 +0100

    Aggiunti miglioramenti

--- blimp_class.py
+++ blimp_class.py
@@ -3,6 +3,8 @@ import warnings
 import numpy as np
 from numpy.linalg import norm
 from quaternion import Quaternion
+import pandas as pd
+from matplotlib import pyplot as plt
 
 class Madgwick:
     samplePeriod = 1/100
@@ -102,19 +104,23 @@ def calibration(magnetic):
     This function is used to calibrate the magnetometer data from Hard
     and sof iron offsets. 
     """
-    magnetic = np.array(magnetic, dtype=float).flatten()
-    mag=[0,0,0]
+    magnetic = np.array(magnetic, dtype=float).flatten() #Convert the measure in a numpy array
+    
+    df = pd.read_csv('data.csv')
+    
+    # Hard Iron Vector
+    b = np.array([df.iloc[0,1], df.iloc[0,2], df.iloc[0,3]])
     #Soft iron transformation matrix:
-    A_1= np.array([[ 5.79424189,  0.34421788, -1.32410436],
-                    [ 0.34421788,  6.40648245,  0.83082061],
-                    [-1.32410436,  0.83082061,  6.40231769]])
+    A_1 = np.array([[df.iloc[1,1], df.iloc[1,2], df.iloc[1,3]],
+                [df.iloc[2,1], df.iloc[2,2], df.iloc[2,3]],
+                [df.iloc[3,1], df.iloc[3,2], df.iloc[3,3]]])
 
+    mag=[0,0,0]
     
     #Hard iron bias:
-
-    xm_off = magnetic[0]-899.775-7.34337263
-    ym_off = magnetic[1]+123.67499999999998-8.06144291
-    zm_off = magnetic[2]-296.325+10.82794456
+    xm_off = magnetic[0]-b[0]
+    ym_off = magnetic[1]-b[1]
+    zm_off = magnetic[2]-b[2]
 
 
     #multiply by the inverse soft iron offset
@@ -123,3 +129,53 @@ def calibration(magnetic):
     mag[2] = (xm_off *  A_1[2,0] + ym_off *  A_1[2,1]  + zm_off *  A_1[2,2])
     
     return mag
+
+
+
+def print_calibration():
+    
+    data = np.loadtxt('data_for_calibration.txt',delimiter=',') #input
+
+
+    plt.rcParams["figure.autolayout"] = True
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(data[:,0], data[:,1], data[:,2], marker='o', color='r')
+
+
+    
+
+    df = pd.read_csv('data.csv')
+    
+    # Hard Iron Vector
+    b = np.array([df.iloc[0,1], df.iloc[0,2], df.iloc[0,3]])
+    #Soft iron transformation matrix:
+    A_1 = np.array([[df.iloc[1,1], df.iloc[1,2], df.iloc[1,3]],
+                [df.iloc[2,1], df.iloc[2,2], df.iloc[2,3]],
+                [df.iloc[3,1], df.iloc[3,2], df.iloc[3,3]]])
+
+    print("Soft iron transformation matrix:\n", A_1)
+    print("Hard iron bias:\n", b)
+
+
+    result = [] 
+    for row in data: 
+    
+        # subtract the hard iron offset
+        xm_off = row[0]-b[0]
+        ym_off  = row[1]-b[1]
+        zm_off  = row[2]-b[2]
+        
+
+        #multiply by the inverse soft iron offset
+        xm_cal = xm_off *  A_1[0,0] + ym_off *  A_1[0,1]  + zm_off *  A_1[0,2] 
+        ym_cal = xm_off *  A_1[1,0] + ym_off *  A_1[1,1]  + zm_off *  A_1[1,2] 
+        zm_cal = xm_off *  A_1[2,0] + ym_off *  A_1[2,1]  + zm_off *  A_1[2,2] 
+
+        result = np.append(result, np.array([xm_cal, ym_cal, zm_cal]) )#, axis=0 )
+        #result_hard_iron_bias = np.append(result, np.array([xm_off, ym_off, zm_off]) )
+
+    result = result.reshape(-1, 3)
+    
+    ax.scatter(result[:,0], result[:,1], result[:,2], marker='o', color='g')
+    plt.show()
